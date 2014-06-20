dd8df87aad88ecddf1245e2447b4438a283cf114
benedictpaten <benedictpaten@gmail.com>
2014-06-20 10:17:14 -0700

    Another fix

--- nanopore/pipeline.sh
+++ nanopore/pipeline.sh
@@ -3,7 +3,7 @@
 #Cleanup the old files
 rm -rf ${2} ${3}
 #Set the python path to just these local directories
-export PYTHONPATH=:../:./submodules
+export PYTHONPATH=:./:./submodules
 #Preferentially put the local binaries at the front of the path
 export PATH=:./submodules/sonLib/bin:./submodules/jobTree/bin:./submodules/bwa/:./submodules/lastz/src/:${PATH}
 python ./nanopore/pipeline.py ${1} --jobTree ${2} --logInfo &> ${3}
