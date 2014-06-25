eef8709ecb5d428fff6195b8b71ace5d0763f5f9
Miten Jain <mitenjain@users.noreply.github.com>
2014-06-25 15:05:11 -0700

    format update
    removed extra tabs

--- nanopore/analyses/qualimap.py
+++ nanopore/analyses/qualimap.py
@@ -5,7 +5,7 @@ import os
 class QualiMap(AbstractMapper):
     def run(self):
     	mapping.sam = self.samFile
-		system("samtools view -Sb %s > %s" % (mapping.sam, mapping.bam))
-		system("samtools sort %s %s" % (mapping.bam, mapping.sorted))
+	system("samtools view -Sb %s > %s" % (mapping.sam, mapping.bam))
+	system("samtools sort %s %s" % (mapping.bam, mapping.sorted))
         system("qualimap/qualimap bamqc -bam -outdir %s %s" % (mapping.sorted.bam, self.outputDir))
 
