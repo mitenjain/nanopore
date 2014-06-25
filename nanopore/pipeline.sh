1c1d14d0ed0c5c394d29492f772088c90ab69157
benedictpaten <benedictpaten@gmail.com>
2014-06-25 09:46:48 -0700

    -

--- nanopore/pipeline.sh
+++ nanopore/pipeline.sh
@@ -3,7 +3,7 @@
 #Cleanup the old files
 rm -rf ${2} ${3}
 #Set the python path to just these local directories
-export PYTHONPATH=:./:./submodules
+export PYTHONPATH=:./:./submodules:${PYTHONPATH}
 #Preferentially put the local binaries at the front of the path
-export PATH=:./submodules/sonLib/bin:./submodules/jobTree/bin:./submodules/bwa/:./submodules/lastz/src/:./submodules/last/src/:${PATH}
+export PATH=:./submodules/sonLib/bin:./submodules/jobTree/bin:./submodules/bwa/:./submodules/lastz/src/:./submodules/last/src/:./submodules/last/scripts/:${PATH}
 python ./nanopore/pipeline.py ${1} --jobTree ${2} --logInfo &> ${3}
