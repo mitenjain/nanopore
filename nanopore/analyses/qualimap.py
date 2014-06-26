373ebf72bc01453af1afe85857bd4b273fae2f4a
Miten Jain <mitenjain@users.noreply.github.com>
2014-06-25 17:30:20 -0700

    format update and debugging done

--- nanopore/analyses/qualimap.py
+++ nanopore/analyses/qualimap.py
@@ -4,8 +4,8 @@ import os
 
 class QualiMap(AbstractAnalysis):
     def run(self):
-    	mapping.sam = self.samFile
-	system("samtools view -Sb %s > %s" % (mapping.sam, mapping.bam))
-	system("samtools sort %s %s" % (mapping.bam, mapping.sorted))
-        system("qualimap bamqc -bam -outdir %s %s" % (mapping.sorted.bam, self.outputDir))
-
+	localBamFile = os.path.join(self.getLocalTempDir(), "mapping.bam")
+	localSortedBamFile = os.path.join(self.getLocalTempDir(), "mapping.sorted")
+	system("samtools view -Sb %s > %s" % (self.samFile, localBamFile))
+	system("samtools sort %s %s" % (localBamFile, localSortedBamFile))
+        system("qualimap bamqc -bam %s -outdir %s" % (localSortedBamFile + ".bam", self.outputDir))
