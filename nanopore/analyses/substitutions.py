b570b8e5b7bd8a0159647255fec9d2133caefe3b
ifiddes <ian.t.fiddes@gmail.com>
2014-07-11 09:34:22 -0700

    added kmer substitution analysis - heatmap is commented out due to failing on sparser matrices as what comes out of test files

--- nanopore/analyses/substitutions.py
+++ nanopore/analyses/substitutions.py
@@ -6,6 +6,21 @@ import xml.etree.cElementTree as ET
 from jobTree.src.bioio import reverseComplement, prettyXml, system
 from collections import Counter
 from math import log
+from itertools import product
+
+class KmerSubstMatrix():
+    def __init__(self, kmer=5):
+        self.matrix = dict()
+        #initialize semi-sparse 1024x1024 matrix of kmers
+        for kmer in product("ATGC", repeat=kmer):
+            self.matrix[kmer] = Counter()
+
+    def addAlignedKmers(self, refKmer, readKmer):
+        self.matrix[refKmer][readKmer] += 1
+
+    def getCount(self, refKmer, readKmer):
+        return self.matrix[refKmer][readKmer]
+
 
 class SubstitutionMatrix():
     """Represents a nucleotide substitution matrix. Also allows 
@@ -28,7 +43,6 @@ class SubstitutionMatrix():
         for b in bases:
             freqs.append(self.getCount(refBase, b))
         return [x / sum(freqs) for x in freqs]
-        #return [-log(x) / sum(freqs) for x in freqs]
     
     def getXML(self):
         def _identity(matches, mismatches):
@@ -55,6 +69,7 @@ class SubstitutionMatrix():
             return 4
         return { 'A':0, 'C':1, 'G':2, 'T':3 }[base]
 
+
 class Substitutions(AbstractAnalysis):
     """Calculates stats on substitutions
     """
@@ -76,9 +91,46 @@ class Substitutions(AbstractAnalysis):
         outf.write("A\tC\tG\tT\n")
         for x in bases:
             freqs = sM.getFreqs(x, bases)
-            outf.write("{}\t{}\n".format(x, "\t".join(map(str,freqs)), "\n"))
+            outf.write("{}\t{}\n".format(x, "\t".join(map(str, freqs)), "\n"))
         outf.close()
         analysis = self.outputDir.split("/")[-2].split("_")[-1] + "_Substitution_Levels"
-
-        #system("Rscript nanopore/analyses/substitution_plot.R {} {} {}".format(os.path.join(self.outputDir, "subst.tsv"), os.path.join(self.outputDir, "substitution_plot.png"), analysis))
         system("Rscript nanopore/analyses/substitution_plot.R {} {} {}".format(os.path.join(self.outputDir, "subst.tsv"), os.path.join(self.outputDir, "substitution_plot.pdf"), analysis))        
+        
+        kmer = 5 #change this to be passed in maybe?
+        kM = KmerSubstMatrix(kmer)
+        sam = pysam.Samfile(self.samFile, "r")
+        for aR in samIterator(sam):
+            refKmer, readKmer = list(), list()
+            for aP in AlignedPair.iterator(aR, refSequences[sam.getrname(aR.rname)], readSequences[aR.qname]): #Walk through the matches mismatches:
+                if aP.getPrecedingReadInsertionLength() == 0 and aP.getPrecedingReadDeletionLength() == 0:
+                    refKmer.append(aP.getRefBase().upper())
+                    readKmer.append(aP.getReadBase().upper())
+                else:
+                    refKmer, readKmer = list(), list() #clear kmers if gaps
+                if len(refKmer) == len(readKmer) == kmer:
+                    #load kmer pair into matrix
+                    kM.addAlignedKmers(tuple(refKmer), tuple(readKmer))
+                    refKmer, readKmer = list(), list()
+        outf = open(os.path.join(self.outputDir, "kmer_subst.tsv"), "w")
+        #build a list of kmers to be tsv header
+        kmers = [x for x in product("ATGC", repeat=kmer)]
+        header = ["".join(x) for x in kmers]
+        outf.write("\t".join(header)); outf.write("\n")
+        for refKmer in kmers:
+            line = ["".join(refKmer)]
+            for readKmer in kmers:
+                #replace kmer match counts with NA so R can ignore
+                if readKmer != refKmer:
+                    line.append(str(kM.getCount(refKmer, readKmer)))
+                else:
+                    line.append("NA")
+            outf.write("\t".join(line)); outf.write("\n")
+        outf.close()
+        #system("Rscript nanopore/analyses/kmer_substitution_plot.R {} {} {} ".format(os.path.join(self.outputDir, "kmer_subst.tsv"), os.path.join(self.outputDir, "kmer_substitution"), analysis))
+
+
+
+
+
+
+
