5c229794d80f63babdcb33e6f30a02c4a41ed318
benedictpaten <benedictpaten@gmail.com>
2014-07-17 13:45:17 -0700

    Adding in blasr realign

--- nanopore/mappers/blasr_params.py
+++ nanopore/mappers/blasr_params.py
@@ -5,3 +5,13 @@ import os
 class BlasrParams(AbstractMapper):
     def run(self):
         system("blasr %s %s -sdpTupleSize 8 -bestn 1 -nproc 8 -sam -out %s -m 0" % (self.readFastqFile, self.referenceFastaFile, self.outputSamFile))
+
+class BlasrParamsChain(BlasrParams):
+    def run(self):
+        BlasrParams.run(self)
+        self.chainSamFile()
+
+class BlasrParamsRealign(BlasrParams):
+    def run(self):
+        BlasrParams.run(self)
+        self.realignSamFile()
