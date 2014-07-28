fc2d0ec9035aeaaf15b9d7b6bef527b99a0df069
benedictpaten <benedictpaten@gmail.com>
2014-07-28 12:41:41 -0400

    Fixing coverage stats to give correct aggregate statistics.

--- nanopore/analyses/coverage.py
+++ nanopore/analyses/coverage.py
@@ -1,12 +1,14 @@
 from nanopore.analyses.abstractAnalysis import AbstractAnalysis
 from nanopore.analyses.utils import AlignedPair, getFastaDictionary, getFastqDictionary, samIterator
 import os
+import numpy
 import pysam
 import xml.etree.cElementTree as ET
 from jobTree.src.bioio import reverseComplement, fastaRead, fastqRead, prettyXml, system
 
 class CoverageCounter:
-    """Counts coverage from a pairwise alignment
+    """Counts coverage from a pairwise alignment.
+    Global alignment means the entire reference and read sequences (trailing indels).
     """
     def __init__(self, readSeqName, refSeqName, globalAlignment=False):
         self.matches = 0
@@ -31,7 +33,7 @@ class CoverageCounter:
             totalReadDeletionLength += aP.getPrecedingReadDeletionLength(self.globalAlignment)
         if self.globalAlignment: #If global alignment account for any trailing indels
             assert len(refSeq) - aP.refPos - 1 >= 0
-            self.totalReadDeletionLength +=  len(refSeq) - aP.refPos - 1
+            self.totalReadDeletionLength += len(refSeq) - aP.refPos - 1
             if alignedRead.is_reverse:
                 totalReadInsertionLength += aP.readPos
             else:
@@ -42,43 +44,59 @@ class CoverageCounter:
         self.totalReadInsertionLength += totalReadInsertionLength
         self.totalReadDeletionLength += totalReadDeletionLength
             
-    def getReadCoverage(self):
+    def readCoverage(self):
         return AbstractAnalysis.formatRatio(self.matches + self.mismatches, self.matches + self.mismatches + self.totalReadInsertionLength)
     
-    def getReferenceCoverage(self):
+    def referenceCoverage(self):
         return AbstractAnalysis.formatRatio(self.matches + self.mismatches, self.matches + self.mismatches + self.totalReadDeletionLength)
     
-    def getAlignmentCoverage(self):
+    def alignmentCoverage(self):
         return AbstractAnalysis.formatRatio(self.matches + self.mismatches, self.matches + self.mismatches + self.totalReadInsertionLength + self.totalReadDeletionLength)
     
-    def getIdentity(self):
+    def identity(self):
         return AbstractAnalysis.formatRatio(self.matches, self.matches + self.mismatches)
     
-    def getReadIdentity(self):
+    def readIdentity(self):
         return AbstractAnalysis.formatRatio(self.matches, self.matches + self.mismatches + self.totalReadInsertionLength)
     
-    def getReferenceIdentity(self):
+    def referenceIdentity(self):
         return AbstractAnalysis.formatRatio(self.matches, self.matches + self.mismatches + self.totalReadDeletionLength)
     
-    def getAlignmentIdentity(self):
+    def alignmentIdentity(self):
         return AbstractAnalysis.formatRatio(self.matches, self.matches + self.mismatches + self.totalReadInsertionLength + self.totalReadDeletionLength)
     
-    def getAlignedReferenceLength(self):
+    def alignedReferenceLength(self):
         return self.matches + self.mismatches + self.totalReadDeletionLength
     
-    def getAlignedReadLength(self):
+    def alignedReadLength(self):
         return self.matches + self.mismatches + self.totalReadInsertionLength
     
     def getXML(self):
-        return ET.Element("coverage", { "refSeqName":self.refSeqName, "readSeqName":self.readSeqName, "readCoverage":str(self.getReadCoverage()), "referenceCoverage":str(self.getReferenceCoverage()), 
-                                "alignmentCoverage":str(self.getAlignmentCoverage()), "identity":str(self.getIdentity()), 
-                                "readIdentity":str(self.getReadIdentity()), "referenceIdentity":str(self.getReferenceIdentity()), 
-                                "alignmentIdentity":str(self.getAlignmentIdentity()),
-                                "alignedReferenceLength":str(self.getAlignedReferenceLength()),
-                                "alignedReadLength":str(self.getAlignedReadLength()),
+        return ET.Element("coverage", { "refSeqName":self.refSeqName, "readSeqName":self.readSeqName, "readCoverage":str(self.readCoverage()), "referenceCoverage":str(self.referenceCoverage()), 
+                                "alignmentCoverage":str(self.alignmentCoverage()), "identity":str(self.identity()), 
+                                "readIdentity":str(self.readIdentity()), "referenceIdentity":str(self.referenceIdentity()), 
+                                "alignmentIdentity":str(self.alignmentIdentity()),
+                                "alignedReferenceLength":str(self.alignedReferenceLength()),
+                                "alignedReadLength":str(self.alignedReadLength()),
                                 "matches":str(self.matches), "mismatches":str(self.mismatches), "ns":str(self.ns), 
                                 "totalReadInsertionLength":str(self.totalReadInsertionLength),
                                 "totalReadDeletionLength":str(self.totalReadDeletionLength) })
+
+def getAggregateCoverageStats(readCoverages, tagName, refSequences, readSequences):
+    """Calculates aggregate stats across a set of read alignments
+    """
+    def stats(fnStringName):
+        l = map(lambda x : getattr(x, fnStringName)(), readCoverages)
+        l.sort()
+        return l[0], numpy.average(l), numpy.median(l), l[-1], " ".join(map(str, l))
+    attribs = { "numberOfReadAlignments":str(len(readCoverages)), "numberOfReads":str(len(readSequences)), "numberOfReferenceSequences":str(len(refSequences)) }
+    for fnStringName in "readCoverage", "referenceCoverage", "alignmentCoverage", "identity", "readIdentity", "referenceIdentity", "alignmentIdentity":
+        for attribName, value in zip([ "min" + fnStringName, "avg" + fnStringName, "median" + fnStringName, "max" + fnStringName, "distribution" + fnStringName ], list(stats(fnStringName))):
+            attribs[attribName] = str(value)
+    parentNode = ET.Element(tagName, attribs)
+    for readCoverage in readCoverages:
+            parentNode.append(readCoverage.getXML())
+    return parentNode
 
 class LocalCoverage(AbstractAnalysis):
     """Calculates coverage, treating alignments as local alignments.
@@ -86,29 +104,28 @@ class LocalCoverage(AbstractAnalysis):
     def run(self, globalAlignment=False):
         refSequences = getFastaDictionary(self.referenceFastaFile) #Hash of names to sequences
         readSequences = getFastqDictionary(self.readFastqFile) #Hash of names to sequences
-        overallCoverageCounter = CoverageCounter("overall", "overall", globalAlignment=globalAlignment) #Thing to store the overall coverage in
-        readCoverages = []
         sam = pysam.Samfile(self.samFile, "r" )
+        readsToReadCoverages = dict([ (readName, []) for readName in readSequences ])
         for aR in samIterator(sam): #Iterate on the sam lines
             refSeq = refSequences[sam.getrname(aR.rname)]
             readSeq = readSequences[aR.qname]
-            overallCoverageCounter.addReadAlignment(aR, refSeq, readSeq)
-            readCoverages.append(CoverageCounter(aR.qname, sam.getrname(aR.rname), globalAlignment=globalAlignment))
-            readCoverages[-1].addReadAlignment(aR, refSeq, readSeq)
+            coverageCounter = CoverageCounter(aR.qname, sam.getrname(aR.rname), globalAlignment=globalAlignment)
+            coverageCounter.addReadAlignment(aR, refSeq, readSeq)
+            readsToReadCoverages[aR.qname].append(coverageCounter)
         sam.close()
-        #Write out the coverage info
-        parentNode = overallCoverageCounter.getXML()
-        for readCoverage in readCoverages:
-            parentNode.append(readCoverage.getXML())
-        open(os.path.join(self.outputDir, "coverages.xml"), 'w').write(prettyXml(parentNode))
-        if len(readCoverages) > 0:
-            outf = open(os.path.join(self.outputDir, "coverages.tsv"), "w")
-            outf.write("alignmentIdentity\talignmentCoverage\treadIdentity\treadCoverage\treferenceIdentity\treferenceCoverage\n")
-            for readCoverage in readCoverages:
-                outf.write("\t".join(map(str, [readCoverage.getAlignmentIdentity(), readCoverage.getAlignmentCoverage(), readCoverage.getReadIdentity(), readCoverage.getReadCoverage(), readCoverage.getReferenceIdentity(), readCoverage.getReferenceIdentity()])))
-                outf.write("\n")
-            outf.close()
-            system("Rscript nanopore/analyses/coverage_plot.R {} {}".format(os.path.join(self.outputDir, "coverages.tsv"), os.path.join(self.outputDir, "coverage_histograms.pdf")))
+        #Write out the coverage info for differing subsets of the read alignments
+        for readCoverages, outputName in [ (reduce(lambda x, y : x + y, readsToReadCoverages.values()), "coverage_all"), (map(lambda x : max(x, key=lambda y : y.readCoverage()), readsToReadCoverages.values()), "coverage_bestPerRead") ]:
+            parentNode = getAggregateCoverageStats(readCoverages, outputName, refSequences, readSequences)
+            open(os.path.join(self.outputDir, outputName + ".xml"), 'w').write(prettyXml(parentNode))
+    
+            if len(readCoverages) > 0:
+                outf = open(os.path.join(self.outputDir, outputName + ".tsv"), "w")
+                outf.write("alignmentIdentity\talignmentCoverage\treadIdentity\treadCoverage\treferenceIdentity\treferenceCoverage\n")
+                for readCoverage in readCoverages:
+                    outf.write("\t".join(map(str, [readCoverage.alignmentIdentity(), readCoverage.alignmentCoverage(), readCoverage.readIdentity(), readCoverage.readCoverage(), readCoverage.referenceIdentity(), readCoverage.referenceIdentity()])))
+                    outf.write("\n")
+                outf.close()
+                system("Rscript nanopore/analyses/coverage_plot.R {} {}".format(os.path.join(self.outputDir, outputName + ".tsv"), os.path.join(self.outputDir, outputName + ".pdf")))
 
 class GlobalCoverage(LocalCoverage):
     def run(self):
