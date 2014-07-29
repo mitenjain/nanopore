e051b3e4e9794fd742bec39bcd18aebc44cf5e3f
benedictpaten <benedictpaten@gmail.com>
2014-07-28 20:56:20 -0400

    Fixing issue of incomplete headers in empty lastz files.

--- nanopore/mappers/lastz.py
+++ nanopore/mappers/lastz.py
@@ -1,9 +1,18 @@
 from nanopore.mappers.abstractMapper import AbstractMapper
-from sonLib.bioio import system
+from sonLib.bioio import system, fastaRead
+import pysam
 
 class Lastz(AbstractMapper):
     def run(self):
         system("lastz %s %s --format=sam > %s" % (self.referenceFastaFile, self.readFastqFile, self.outputSamFile))
+        try:
+            pysam.Samfile(self.outputSamFile, "r" ).close()
+        except ValueError:
+            #Hack to make lastz work, creating SQ lines when no alignments are found
+            fH = open(self.outputSamFile, 'a')
+            for name, seq in fastaRead(open(self.referenceFastaFile, 'r')):
+                fH.write("@SQ\tSN:%s\tLN:%s\n" % (name.split()[0], len(seq)))
+            fH.close()
         
 class LastzChain(Lastz):
     def run(self):
