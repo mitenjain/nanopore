834dee77aba75cd9a9ca1291cea6af2609b92d16
benedictpaten <benedictpaten@gmail.com>
2014-08-01 00:37:41 -0400

    -

--- nanopore/mappers/blasr.py
+++ nanopore/mappers/blasr.py
@@ -1,10 +1,25 @@
 from nanopore.mappers.abstractMapper import AbstractMapper
+from nanopore.analyses.utils import getFastqDictionary
 from sonLib.bioio import system
 import os
 
 class Blasr(AbstractMapper):
     def run(self):
-        system("blasr %s %s -sam > %s" % (self.readFastqFile, self.referenceFastaFile, self.outputSamFile))
+        tempSamFile = os.path.join(self.getLocalTempDir(), "temp.sam")
+        system("blasr %s %s -sam > %s" % (self.readFastqFile, self.referenceFastaFile, tempSamFile))
+        #Blasr seems to corrupt the names of read sequences, so lets correct them.
+        sam = pysam.Samfile(tempSamFile, "r" )
+        outputSam = pysam.Samfile(self.outputSamFile, "w", template=samfile)
+        readSequences = getFastqDictionary(self.readFastqFile) #Hash of names to sequences
+        for aR in samIterator(sam): #Iterate on the sam lines and put into buckets by read
+            if aR.qname not in readSequences:
+                newName = '/'.join(aR.qname.split('/')[:-1])
+                if newName not in readSequences:
+                    raise RuntimeError("Tried to deduce correct read name: %s, %s" % (newName, readSequences.keys()))
+                aR.qname = newName
+            outputSam.write(aR)
+        outputSam.close()
+        
 
 class BlasrChain(Blasr):
     def run(self):
