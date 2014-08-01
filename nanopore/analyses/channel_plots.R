750a93ba302e41a7a2bbb2c5df6b97db4edc3db3
ifiddes <ian.t.fiddes@gmail.com>
2014-08-01 16:46:00 -0700

    bugs, added a new plot

--- nanopore/analyses/channel_plots.R
+++ nanopore/analyses/channel_plots.R
@@ -4,11 +4,15 @@ args <- commandArgs(trailingOnly = T)
 
 data <- read.table(args[1], row.names=1, header=T)
 
-if (dim(data[1] > 1)) {
+if (dim(data)[1] > 1) {
     
     pdf(args[2])
 
-    barplot(t(data)[2,]/(t(data[1,]+t(data)[2,])*100, main="% Mappable Reads Per Channel", xlab="Channel", ylab="% Mappable")
+    sorted <- data[order(data$ReadCount, decreasing=T),]
+    q<- barplot(as.matrix(t(sorted)), main="Sorted Channel Mappability", xlab="Channel", ylab="Read Counts", legend.text=T, xaxt="n")
+    text(cex=0.2, x=q-.25,y=-1.25, rownames(sorted), xpd=T, srt=45)
+
+    barplot(t(data)[2,]/(t(data)[1,]+t(data)[2,])*100, main="% Mappable Reads Per Channel", xlab="Channel", ylab="% Mappable")
 
     par(mfrow=(c(1,2)))
 
