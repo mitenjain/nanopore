e4811eee6631ac16d719120a5fc9e92e1795f1d6
ifiddes <ian.t.fiddes@gmail.com>
2014-08-13 14:36:09 -0700

    new version of pipeline that treats folders in the read folder as different read types to be analyzed separately

--- nanopore/metaAnalyses/abstractMetaAnalysis.py
+++ nanopore/metaAnalyses/abstractMetaAnalysis.py
@@ -3,10 +3,11 @@ from jobTree.scriptTree.target import Target
 class AbstractMetaAnalysis(Target):
     """Base class to for meta-analysis targets. Inherit this class to create a meta-analysis.
     """
-    def __init__(self, outputDir, experiments):
+    def __init__(self, outputDir, readType, experiments):
         Target.__init__(self)
         self.experiments = experiments
         self.outputDir = outputDir
+        self.readType = readType
         
         #Quadruples of (readFastqFile, readType, referenceFastaFile, mapper) to pairs of (analyses, resultsDir)
         self.experimentHash = {}
