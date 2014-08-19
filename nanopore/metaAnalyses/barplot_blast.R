5a2a7197099d3b202c48446038b06c43cfa20143
ifiddes <ian.t.fiddes@gmail.com>
2014-08-19 14:24:22 -0700

    more fixes to blast

--- nanopore/metaAnalyses/barplot_blast.R
+++ nanopore/metaAnalyses/barplot_blast.R
@@ -5,6 +5,6 @@ nums <- read.table(args[1])
 
 if (length(nums[,1]) >= 1) {
     pdf(args[2])
-    barplot(nums, names.arg=c("Blast Hits", "Mapped","Unapped"), col=c("red","blue","purple"), main=paste(args[3], " # Of Mapped and Unmapped Reads"))
+    barplot(nums[,1], names.arg=c("Blast Hits", "Mapped","Unmapped"), col=c("red","blue","purple"), main=paste(args[3], " # Of Mapped and Unmapped Reads"))
     dev.off()
 }
