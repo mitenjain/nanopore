934e4f1f49843135494145bb73eab1a3c0ed1d1d
ifiddes <ian.t.fiddes@gmail.com>
2014-08-28 16:17:12 -0700

    error in unmapped length distribution

--- nanopore/metaAnalyses/unmappedLengthDistributionAnalysis.py
+++ nanopore/metaAnalyses/unmappedLengthDistributionAnalysis.py
@@ -31,7 +31,7 @@ class UnmappedLengthDistributionAnalysis(AbstractUnmappedMetaAnalysis):
                 else:
                     unmapped.write("{}\n".format(len(read.seq)))
             unmapped.close(); mapped.close()
-            if os.path.getsize(os.path.join(self.getLocalTempDir(), readType + "_unmapped")) > 0 and os.path.getsize(os.path.join(self.getLocalTempDir(), readType + "_mapped")) > 0:
+            if os.path.getsize(os.path.join(self.getLocalTempDir(), os.path.basename(reference) + "_unmapped")) > 0 and os.path.getsize(os.path.join(self.getLocalTempDir(), os.path.basename(reference) + "_mapped")) > 0:
                 system("Rscript nanopore/metaAnalyses/unmapped_mapped_distributions.R {} {} {} {}".format(os.path.join(self.getLocalTempDir(), os.path.basename(reference) + "_unmapped"), os.path.join(self.getLocalTempDir(), os.path.basename(reference) + "_mapped"), os.path.join(self.outputDir, os.path.basename(reference) + "_length_distribution.pdf"), os.path.basename(reference)))
             else:
                 os.remove(os.path.join(self.getLocalTempDir(), readType + "_unmapped"))
