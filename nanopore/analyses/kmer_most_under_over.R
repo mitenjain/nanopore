57a254f989b7b045d7acb219fc22e1007790f5d6
ifiddes <ian.t.fiddes@gmail.com>
2014-08-29 10:58:58 -0700

    making whitespaces consistent

--- nanopore/analyses/kmer_most_under_over.R
+++ nanopore/analyses/kmer_most_under_over.R
@@ -3,12 +3,12 @@
 args <- commandArgs(trailingOnly = T)
 
 if (file.info(args[1])$size != 0) {
-	kmers <- read.table(args[1])
-	colnames(kmers) <- c("Kmer", "refCount", "RefFreq", "ReadCount", "ReadFreq", "LogRatio", "RefFreq", "LogRatioAgain")
+    kmers <- read.table(args[1])
+    colnames(kmers) <- c("Kmer", "refCount", "RefFreq", "ReadCount", "ReadFreq", "LogRatio", "RefFreq", "LogRatioAgain")
 
-	top <- head(kmers[order(kmers$LogRatio),], n=10L)
-	bot <- head(kmers[order(kmers$LogRatio, decreasing=T),], n=10L)
+    top <- head(kmers[order(kmers$LogRatio),], n=10L)
+    bot <- head(kmers[order(kmers$LogRatio, decreasing=T),], n=10L)
 
-	write.table(top[,c(-7,-8)], file=args[2], sep="\t")
-	write.table(bot[,c(-7,-8)], file=args[3], sep="\t")
+    write.table(top[,c(-7,-8)], file=args[2], sep="\t")
+    write.table(bot[,c(-7,-8)], file=args[3], sep="\t")
 }
--- nanopore/analyses/match_hist.R
+++ nanopore/analyses/match_hist.R
@@ -6,10 +6,10 @@ data <- read.table(args[1], sep="\t")
 
 if ( dim(data)[1] > 0 ) {
 
-	pdf(args[2])
+    pdf(args[2])
 
-	hist(t(data), main = "Average Posterior Match Probability", xlab="Probability")
+    hist(t(data), main = "Average Posterior Match Probability", xlab="Probability")
 
-	dev.off()
+    dev.off()
 
 }
