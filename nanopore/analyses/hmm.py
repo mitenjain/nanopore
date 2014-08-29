e0e6a88b37c2f820465d39b2ab3785aa871924fe
ifiddes <ian.t.fiddes@gmail.com>
2014-08-29 09:43:09 -0700

    I thought I fixed this last night...

--- nanopore/analyses/hmm.py
+++ nanopore/analyses/hmm.py
@@ -75,7 +75,7 @@ class Hmm(AbstractAnalysis):
             outf.write("\t".join(map(lambda x : str(deleteEmissions[x]), bases)) + "\n")
             outf.close()
             ###Here's where we do the plot..
-            system("Rscript nanopore/metaAnalyses/emissions_plot.R {} {}".format(indelEmissionsFile, os.path.join(self.outputDir, "indelEmissions_plot.pdf")))
+            system("Rscript nanopore/analyses/emissions_plot.R {} {}".format(indelEmissionsFile, os.path.join(self.outputDir, "indelEmissions_plot.pdf")))
 
             #Plot convergence of likelihoods
             outf = open(os.path.join(self.outputDir, "runninglikelihoods.tsv"), "w")
@@ -83,6 +83,6 @@ class Hmm(AbstractAnalysis):
                 runningLikelihoods = map(float, hmmNode.attrib["runningLikelihoods"].split()) #This is a list of floats ordered from the first iteration to last.
                 outf.write("\t".join(map(str, runningLikelihoods))); outf.write("\n")
             outf.close()
-            system("Rscript nanopore/metaAnalyses/running_likelihood.R {} {}".format(os.path.join(self.outputDir, "runninglikelihoods.tsv"), os.path.join(self.outputDir, "running_likelihood.pdf")))
+            system("Rscript nanopore/analyses/running_likelihood.R {} {}".format(os.path.join(self.outputDir, "runninglikelihoods.tsv"), os.path.join(self.outputDir, "running_likelihood.pdf")))
             
         self.finish() #Indicates the batch is done
