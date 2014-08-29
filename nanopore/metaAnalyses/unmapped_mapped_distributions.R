bc90431bc570a02ec78d8c38c95547e79bfa952f
ifiddes <ian.t.fiddes@gmail.com>
2014-08-29 10:21:54 -0700

    removing length outliers in unmapped plots

--- nanopore/metaAnalyses/unmapped_mapped_distributions.R
+++ nanopore/metaAnalyses/unmapped_mapped_distributions.R
@@ -7,8 +7,14 @@ analysis <- args[4]
 
 if (length(unmapped[,1]) >= 1) {
     pdf(args[3])
-    mapped.density <- density(mapped[,1])
-    unmapped.density <- density(unmapped[,1])
+    
+    #remove outliers so graph is prettier
+    repoutliers <- function(x){ med=median(x); mad=mad(x); x[x>med+3*mad | x<med-3*mad]=NA; return(x)}
+    mapped <- apply(mapped, 2, repoutliers)
+    unmapped <- apply(unmapped, 2, repoutliers)
+
+    mapped.density <- density(mapped[,1], na.rm=T)
+    unmapped.density <- density(unmapped[,1], na.rm=T)
     xmax <- max(unmapped.density$x, mapped.density$x)
     ymax <- max(unmapped.density$y, mapped.density$y)
     plot(mapped.density, xlim=c(0,xmax), ylim=c(0,ymax), xlab="Read Length", main=paste(analysis, "Mappable and Unmappable Read Lengths", sep=" "), col="blue")
