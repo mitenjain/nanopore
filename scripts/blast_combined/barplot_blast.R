2cf51a812ea2ba6a40eb48f57a7fbd64e643e4e3
ifiddes <ian.t.fiddes@gmail.com>
2014-09-03 15:40:14 -0700

    re added mapped reads to blast barplot

--- scripts/blast_combined/barplot_blast.R
+++ scripts/blast_combined/barplot_blast.R
@@ -7,7 +7,7 @@ mapped <- args[3]
 
 if (length(nums[,1]) >= 1) {
     pdf(args[3])
-    q <- barplot(c(blast, unmapped, mapped)), names.arg=c("Blast Hits", "Still Unmapped"), col=c("red","blue"), main="Percent of Reads"))
+    q <- barplot(c(blast, unmapped, mapped)), names.arg=c("Blast Hits", "Still Unmapped", "Originally Mapped"), col=c("red","blue"), main="Percent of Reads"))
     text(y=c(blast, unmapped, mapped)+0.015, x=q, labels=paste(as.character(round(c(blast, unmapped, mapped)*100, 3)), "%"), xpd=T)
     dev.off()
 }
