bfbeaf96d6ad269d3c69c46658c5a920d6ed0245
ifiddes <ian.t.fiddes@gmail.com>
2014-09-03 15:55:14 -0700

    added second version of running likelihood plot

--- nanopore/analyses/running_likelihood.R
+++ nanopore/analyses/running_likelihood.R
@@ -10,15 +10,25 @@ tryCatch({
 
     if (dim(dist)[1] > 1) {
 
+        m <- max(dist)
+
         pdf(out)
 
         r <- topo.colors(length(rownames(dist)))
         n <- 0
-        plot(x=seq(1,dim(dist)[2]),y=as.vector(dist[1,]/max(dist[1,])), type="n", main="Convergence of Likelihoods", xlab="Trials", ylab="Running Likelihoods")
+        plot(x=seq(1,dim(dist)[2]),y=as.vector(dist[1,]/m), type="n", main="Convergence of Likelihoods", xlab="Trials", ylab="Running Log Likelihoods Ratio")
         for (i in 1:length(rownames(dist))) {
             n <- n + 1
-            lines(x=seq(1,dim(dist)[2]),y=as.vector(dist[i,]/max(dist[i,])), col =r[n], lty=n%%2)
+            lines(x=seq(1,dim(dist)[2]),y=as.vector(dist[i,]/m), col =r[n])
         }
+
+        n <- 0
+        plot(x=seq(1,dim(dist)[2]),y=log(as.vector(dist[1,]/m)), type="n", main="Convergence of Likelihoods", xlab="Trials", ylab="Running Log-Log Likelihoods Ratio", ylim=c(0,0.01))
+        for (i in 1:length(rownames(dist))) {
+            n <- n + 1
+            lines(x=seq(1,dim(dist)[2]),y=log(as.vector(dist[i,]/m)), col =r[n])
+        }
+
         dev.off()
     }
 }, error = function(err) { 
