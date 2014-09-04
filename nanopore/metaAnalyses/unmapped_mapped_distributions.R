23b292d3d0231930da4587f185899a5df2ca7233
ifiddes <ian.t.fiddes@gmail.com>
2014-09-04 09:25:31 -0700

    adding stacked histograms to unmapped meta analysis

--- nanopore/metaAnalyses/unmapped_mapped_distributions.R
+++ nanopore/metaAnalyses/unmapped_mapped_distributions.R
@@ -21,8 +21,19 @@ if (length(unmapped[,1]) >= 1) {
     lines(unmapped.density, col="red")
     legend(x="topright", col=c("blue","red"), legend=c("Mapped","Unmapped"), pch="-")
 
+    m <- hist(mapped, breaks = "FD", plot=F)
+    u <- hist(unmapped, breaks = "FD", plot=F)
+    combined <- cbind(mapped, unmapped)
+    combined.sort <- combined[order(combined)]
+    xmax <- combined.sort[round(0.95*length(combined.sort))]
+    ymax <- max(m$counts, u$counts)
+    plot(m, col=rgb(1,0,0,0.5), xlim=c(0,xmax), ylim=c(0,ymax), main="Mapped and Unmapped Read Length Distributions", xlab="Read Length")
+    plot(u, col=rgb(0,0,1,0.5), add=T, xlim=c(0,xmax), ylim=c(0,ymax), main="", xlab="", ylab="")
+    legend("topleft", pch=15, legend=c("Mapped", "Unmapped"), col=c(rgb(1,0,0),rgb(0,0,1)))
+
     lengths <- c(length(unmapped[,1]), length(mapped[,1]))
     barplot(lengths, names.arg=c("Unmapped","Mapped"), col=c("red","blue"), main=paste(analysis, " # Of Mapped and Unmapped Reads"))
+
 
     dev.off()
 }
