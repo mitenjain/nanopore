cebe5e5b175b229c49a7e5f627fdf8eb4fcfe179
ifiddes <ian.t.fiddes@gmail.com>
2014-09-03 22:58:54 -0700

    changed text to legend in summary plots

--- nanopore/metaAnalyses/coverageSummaryPlots.R
+++ nanopore/metaAnalyses/coverageSummaryPlots.R
@@ -18,12 +18,11 @@ if (dim(summary)[1] >= 1) {
     ################################################
     
     plot(summary$AvgInsertionsPerReadBase, summary$AvgDeletionsPerReadBase, xlab="Avg. Insertions Per Aligned Read Base", ylab="Avg. Deletions Per Aligned Read Base", main=name, col=r, pch=c(19,18,15,17), xlim=c(0,0.2), ylim=c(0,0.2), cex.main=0.9)
-    text(summary$AvgInsertionsPerReadBase, summary$AvgDeletionsPerReadBase, cex=0.75, pos=2, labels=rownames(summary))
-    #legend(x="top", legend=rownames(summary), col=r, pch=c(19,18,15,17), cex=0.75)
+    legend("topright") cex=0.75 legend=rownames(summary), pch=c(19,18,15,17), col=r)
     
     #scatterplot of Avg identity coverage vs Avg # of indels per base
     plot(100 * summary$AvgMatchIdentity, summary$AvgDeletionsPerReadBase + summary$AvgInsertionsPerReadBase, ylab="Avg. Indels Per Aligned Read Base", xlab="Avg. Match Identity", main=name, col=r, pch=20, xlim=c(0,100), cex.main=0.9)
-    text(100 * summary$AvgMatchIdentity, summary$AvgDeletionsPerReadBase + summary$AvgInsertionsPerReadBase, cex=0.75, pos=2, labels=rownames(summary))
+    legend("topright") cex=0.75 legend=rownames(summary), pch=c(19,18,15,17), col=r)
 
     q <- barplot(height=summary$AvgInsertionsPerReadBase, xaxt="n", col=r, main=paste(name,"Avg. Insertions Per Aligned Base",sep="\n"), ylab="Avg. Insertions Per Aligned Base", cex.main=0.9)
     text(cex=0.8, x=q, y=-0.01, rownames(summary), xpd=T, srt=90)
@@ -43,7 +42,7 @@ if (dim(summary)[1] >= 1) {
     
     #scatterplot of Avg read coverage vs Avg match identity
     plot(100 * summary$AvgReadCoverage, 100 * summary$AvgMatchIdentity, ylab="Avg. Match Identity", xlab="Avg. Read Coverage", main=name, col=r, pch=20, xlim=c(0,100), ylim=c(0,100), cex.main=0.9)
-    text(100 * summary$AvgReadCoverage, 100 * summary$AvgMatchIdentity, cex=0.75, pos=2, labels=rownames(summary))
+    legend("topright") cex=0.75 legend=rownames(summary), pch=c(19,18,15,17), col=r)
 
     ################################################
     ####Looking at unmapped reads - demonstrating last is doing a great job
