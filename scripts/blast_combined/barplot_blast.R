8bcf24589e409dace136b0893b1502871263e4c5
ifiddes <ian.t.fiddes@gmail.com>
2014-09-04 11:23:54 -0700

    colors

--- scripts/blast_combined/barplot_blast.R
+++ scripts/blast_combined/barplot_blast.R
@@ -7,6 +7,6 @@ mapped <- round(as.numeric(args[3]), 3)
 readType <- args[4]
 
 pdf(args[5])
-q <- barplot(c(blast, unmapped, mapped), names.arg=c("Blast Hits", "Still Unmapped", "Originally Mapped"), col=c("red","blue"), main=paste(readType, "Percent of Reads", sep=" "))
+q <- barplot(c(blast, unmapped, mapped), names.arg=c("Blast Hits", "Still Unmapped", "Originally Mapped"), col=c("red","blue","green"), main=paste(readType, "Percent of Reads", sep=" "))
 text(y=c(blast, unmapped, mapped)+0.02, x=q, labels=paste(as.character(round(c(blast, unmapped, mapped)*100, 3)), "%"), xpd=T)
 dev.off()
