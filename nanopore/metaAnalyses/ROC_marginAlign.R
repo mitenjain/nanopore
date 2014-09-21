a7673406d42b37c8b970ec43760796551f387d1f
ifiddes <ian.t.fiddes@gmail.com>
2014-09-21 10:23:47 -0700

    ROC fixes

--- nanopore/metaAnalyses/ROC_marginAlign.R
+++ nanopore/metaAnalyses/ROC_marginAlign.R
@@ -3,18 +3,18 @@
 args <- commandArgs(trailingOnly = T)
 library(lattice)
 
-data <- read.table(args[1], row.names=1)
+data <- read.table(args[1])
 outf <- args[2]
 cols <- as.numeric(args[3])
 rows <- as.numeric(args[4])
 #set it to variant calling algorithms x proportion held out plots
 par(mfrow=c(cols, rows))
 
-fprs <- data[seq(1, length(data), 3)]
-tprs <- data[seq(2, length(data), 3)]
+fprs <- data[seq(1, length(data), 2)]
+tprs <- data[seq(2, length(data), 2)]
 
 for (i in 1:dim(fprs)[1]) {
-    xyplot(tprs[i,]~fpr[i,], xlab="False positive rate", ylab="True positive rate", main=rowname(fprs[i,]), type="l", xlim=c(0,1), ylim=c(0,1))
+    xyplot(tprs[i,]~fpr[i,], xlab="False positive rate", ylab="True positive rate", main=paste("variantCaller:\n", fprs[i,][1], "\nProportion held out: ", fprs[i,][2], sep=" "), type="l", xlim=c(0,1), ylim=c(0,1))
 
 }
 
