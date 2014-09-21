7447de057003a0f9c4778069a455b1a353a54da8
ifiddes <ian.t.fiddes@gmail.com>
2014-09-21 10:24:53 -0700

    ROC fixes

--- nanopore/metaAnalyses/ROC_marginAlign.R
+++ nanopore/metaAnalyses/ROC_marginAlign.R
@@ -14,7 +14,7 @@ fprs <- data[seq(1, length(data), 2)]
 tprs <- data[seq(2, length(data), 2)]
 
 for (i in 1:dim(fprs)[1]) {
-    xyplot(tprs[i,]~fpr[i,], xlab="False positive rate", ylab="True positive rate", main=paste("variantCaller:\n", fprs[i,][1], "\nProportion held out: ", fprs[i,][2], sep=" "), type="l", xlim=c(0,1), ylim=c(0,1))
+    xyplot(tprs[i,][3:length(tprs[i,])]~fpr[i,][3:length(tprs[i,])], xlab="False positive rate", ylab="True positive rate", main=paste("variantCaller:\n", fprs[i,][1], "\nProportion held out: ", fprs[i,][2], sep=" "), type="l", xlim=c(0,1), ylim=c(0,1))
 
 }
 
