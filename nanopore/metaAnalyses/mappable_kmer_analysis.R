f428a952514586b6c88110ab6d83f130a992c2c2
ifiddes <ian.t.fiddes@gmail.com>
2014-09-21 07:31:25 -0700

    fix

--- nanopore/metaAnalyses/mappable_kmer_analysis.R
+++ nanopore/metaAnalyses/mappable_kmer_analysis.R
@@ -17,8 +17,8 @@ for (i in 1:1024) {
 
 #10,000 trials
 num_trials <- 10000
-#we want each trial to be around 1/10th of the number of kmers seen in the reads
-trial_size <- round(length(counts)/10)
+#we want each trial to be around 1/25th of the number of kmers seen in the reads
+trial_size <- max(round(length(counts)/25), 10000)
 
 
 #samples from the read population
