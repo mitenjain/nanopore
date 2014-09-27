59c64da5d741020c1cbe7534406231cf0fa2a2dc
benedictpaten <benedictpaten@gmail.com>
2014-09-27 12:56:01 -0700

    -

--- nanopore/pipeline.py
+++ nanopore/pipeline.py
@@ -81,9 +81,9 @@ analyses = [ Hmm, GlobalCoverage, LocalCoverage, Substitutions, Indels, Alignmen
 metaAnalyses = [ UnmappedKmerAnalysis, CoverageSummary, UnmappedLengthDistributionAnalysis, ComparePerReadMappabilityByMapper ]# CustomTrackAssemblyHub ]
 
 
-analyses = [ MarginAlignSnpCaller ]
-metaAnalyses = [ MarginAlignMetaAnalysis  ] 
-mappers = [LastParamsRealignEm  ]
+#analyses = [ MarginAlignSnpCaller ]
+#metaAnalyses = [ MarginAlignMetaAnalysis  ] 
+#mappers = [LastParamsRealignEm  ]
 
 #The following runs the mapping and analysis for every combination of readFastqFile, referenceFastaFile and mapper
 def setupExperiments(target, readFastqFiles, referenceFastaFiles, mappers, analysers, metaAnalyses, outputDir):
