e4224f1956712780ca477c273bf00427b4dd9e3c
ifiddes <ian.t.fiddes@gmail.com>
2014-09-27 15:50:29 -0700

    more changes to kmer analysis, new design approach to significance testing

--- nanopore/analyses/kmer_analysis.R
+++ nanopore/analyses/kmer_analysis.R
@@ -8,31 +8,29 @@ outsig <- args[3]
 library(stats)
 
 
-#10,000 trials
 num_trials <- 5000
-#we want each trial to be around 1/40th of the number of kmers seen in the reads
-trial_size <- round(sum(data$readCount/40))
+#trial size is a max of 10,000
+if (sum(data$refCount/50) > 10000) {
+    trial_size <- 10000
+} else {
+    trial_size <- sum(data$refCount)/50
+}
 
-#initialize empty vector of trial success counts
-counts <- rep(0, 1024)
-#samples from the read population
-trial_fn <- function(data) {
-    #sample from the population with replacement so this doesn't take years to process
-    tmp <- matrix(table(factor(sample(1024, trial_size, prob=data$readFraction, replace=T), levels=1:1024)))[,1]
-    #round the result for floating point problems
-    round(tmp/sum(tmp),8) == round(data$refFraction,8)
+
+trial_fn <- function(d, t) {
+    matrix(table(factor(sample(1024, t, prob=d, replace=T), levels=1:1024)))[,1]
 }
-#do num_trials trials, saving the number of successes for each kmer along the way
-for (i in 1:num_trials){
-    counts <- counts + trial_fn(data)
-}
-#initialize an empty vector of p-valuies
+
+ref <- replicate(num_trials, trial_fn(d=data$refFraction, t=trial_size))
+
+read <- replicate(num_trials, trial_fn(d=data$readFraction, t=trial_size))
+
 p_values <- rep(0, 1024)
-#loop over each kmer, count the number of successful trials, then run a binomial test on that
-for (kmer in 1:1024) {
-    p_values[kmer] <- binom.test(counts[kmer], num_trials, data$refFraction[kmer], alternative="two.sided", conf.level=0.95)$p.value
+
+for (i in 1:1024) {
+    p_values[i] <- t.test(ref[i,], read[i,])$p.value
 }
-#do a bonferroni correction on the pvalues
+
 adjusted_p_value <- p.adjust(p_values, method="bonferroni")
 #combine original data frame with two new vectors
 finished <- cbind(data, p_values, adjusted_p_value)
@@ -44,6 +42,6 @@ significant <- finished[finished$adjusted_p_value <= 0.05,]
 ordered <- significant[order(significant$logFoldChange),]
 #report the top 20 and bottom 20 significant hits
 top <- head(ordered, n=20L)
-bot <- tail(ordered, n=20L)
+bot <- apply(tail(ordered, n=20L), 2, rev)
 
 write.table(rbind(top,bot), outsig)
--- nanopore/metaAnalyses/mappable_kmer_analysis.R
+++ nanopore/metaAnalyses/mappable_kmer_analysis.R
@@ -8,29 +8,29 @@ outsig <- args[3]
 library(stats)
 
 
-#10,000 trials
 num_trials <- 5000
-#we want each trial to be around 1/50th of the number of kmers seen in the mappable
-trial_size <- round(sum(data$mappableCount/50))
-
-
-counts <- rep(0, 1024)
-#samples from the read population
-trial_fn <- function(data) {
-    tmp <- matrix(table(factor(sample(1024, trial_size, prob=data$mappableFraction, replace=T), levels=1:1024)))[,1]
-    round(tmp/sum(tmp),4) == round(data$unmappableFraction,4)
+#trial size is a max of 10,000
+if (sum(data$unmappableCount/50) > 10000) {
+    trial_size <- 10000
+} else {
+    trial_size <- sum(data$unmappableCount)/50
 }
 
-for (i in 1:num_trials){
-    counts <- counts + trial_fn(data)
+
+trial_fn <- function(d, t) {
+    matrix(table(factor(sample(1024, t, prob=d, replace=T), levels=1:1024)))[,1]
 }
+
+mappable <- replicate(num_trials, trial_fn(d=data$mappableFraction, t=trial_size))
+
+unmappable <- replicate(num_trials, trial_fn(d=data$unmappableFraction, t=trial_size))
 
 p_values <- rep(0, 1024)
-#loop over each kmer, count the number of successful trials, then run a binomial test on that
-for (kmer in 1:1024) {
-    p_values[kmer] <- binom.test(counts[kmer], num_trials, data$unmappableFraction[kmer], alternative="two.sided", conf.level=0.95)$p.value
+
+for (i in 1:1024) {
+    p_values[i] <- t.test(mappable[i,], unmappable[i,])$p.value
 }
-#do a bonferroni correction on the pvalues
+
 adjusted_p_value <- p.adjust(p_values, method="bonferroni")
 #combine original data frame with two new vectors
 finished <- cbind(data, p_values, adjusted_p_value)
@@ -42,6 +42,6 @@ significant <- finished[finished$adjusted_p_value <= 0.05,]
 ordered <- significant[order(significant$logFoldChange),]
 #report the top 20 and bottom 20 significant hits
 top <- head(ordered, n=20L)
-bot <- tail(ordered, n=20L)
+bot <- apply(tail(ordered, n=20L), 2, rev)
 
 write.table(rbind(top,bot), outsig)
