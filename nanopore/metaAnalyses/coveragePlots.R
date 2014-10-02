a84ec767c483c79c3fefd36ee9858d1356150dbd
ifiddes <ian.t.fiddes@gmail.com>
2014-10-02 13:10:25 -0700

    sorry benedict

--- nanopore/metaAnalyses/coveragePlots.R
+++ nanopore/metaAnalyses/coveragePlots.R
@@ -51,59 +51,41 @@ if (! is.null(dim(dist)) && dim(dist)[2] > 2 && dim(dist)[1] > 1) {
     #hacky way to determine the number of replicates seen
     num <- 1
     for (i in 1:length(rownames(dist))){
-        num <- as.numeric(strsplit(rownames(dist)[i], "[.]")[[1]][2])
-        if (! is.na(num)) {
-            times <- max(times, num)
+        tmp <- as.numeric(strsplit(rownames(dist)[i], "[.]")[[1]][2])
+        if (! is.na(tmp)) {
+            num <- max(tmp, num)
         }
     }
+    if (num > 1) {
+        #this plot plots the replicates as the same color with different pch and one (average) line
+        #build a new color map where we have one color for every aligner group
+        colmap <- vector()
+        for (i in 1:8){
+            colmap <- c(colmap, rep(i, times=num))
+        }
+        pchmap <- rep(15:(15+num-1), times=ceiling(length(hists)/num))
 
-    #this plot plots the replicates as the same color with different pch and one (average) line
-    #build a new color map where we have one color for every aligner group
-    colmap <- vector()
-    for (i in 1:8){
-        colmap <- c(colmap, rep(i, times=num))
+        plot(1, xlim=c(0,xmax), ylim=c(0,ymax), main=paste(args[2],"Identity by Mapper", sep="\n"), xlab="Identity", type="n", ylab="Frequency")
+        
+        for (i in seq(num, length(hists), num)) {
+            for (j in 0:(num-1)) {
+                x <- hists[[i-j]]
+            points(x$mids, x$counts, col=colmap[i-j], pch=pchmap[i-j], cex=0.4)
+            lines(x$mids, x$counts, col=colmap[i-j])
+            }
+        }   
+
+        avgs <- vector()
+        names <- vector()
+        for (i in seq(num, length(hists), num)){
+            tmp <- mean(c(dist[i,][!is.na(dist[i,])], dist[i-1,][!is.na(dist[i-1,])], dist[i-2,][!is.na(dist[i-2,])]))
+            tmp <- paste(formatC(round(100*tmp,1),format="f",digits=1), sep= "")
+            avgs <- c(avgs, tmp)
+            names <- c(names, rownames(dist)[i-2])
+        }
+
+        coltmp <- rep(c(1:length(names), rep("NA", times=length(names))), times=10)
+        legend("top", col=coltmp, pch="-", cex=0.55, legend=cbind(names, avgs), ncol=2, title="\t\tAverage % Identity", bty="n")
     }
-    pchmap <- rep(15:(15+num-1), times=ceiling(length(hists)/num))
-
-    #build these bins to force each replicate to have the same midpoints
-    bins <- seq(0:(b-1))/b
-    bins <- bins[bins <= xmax]
-
-
-    #force each replicate to have the same midpoints
-    hists2 <- list()
-    for (i in 1:length(rownames(dist))){
-        hists2[[i]] <- hist(dist[i,][!is.na(dist[i,])], plot=F, breaks=bins)
-        xmax <- max(xmax, hists2[[i]]$mids)
-        ymax <- max(ymax, hists2[[i]]$counts)
-    }    
-
-
-    plot(1, xlim=c(0,xmax), ylim=c(0,ymax), main=paste(args[2],"Identity by Mapper", sep="\n"), xlab="Identity", type="n", ylab="Frequency")
-    
-    for (i in seq(num, length(hists2), num)) {
-        x <- hists2[[i]]
-        y <- hists2[[i-1]]
-        z <- hists2[[i-2]]
-        points(z$mids, z$counts, col=colmap[i-2], pch=pchmap[i-2], cex=0.4)
-        points(y$mids, y$counts, col=colmap[i-1], pch=pchmap[i-1], cex=0.4)
-        points(x$mids, x$counts, col=colmap[i], pch=pchmap[i], cex=0.4)
-        lines(z$mids, z$counts, col=colmap[i-2])
-        lines(y$mids, y$counts, col=colmap[i-1])
-        lines(x$mids, x$counts, col=colmap[i])
-    }
-
-    avgs <- vector()
-    names <- vector()
-    for (i in seq(num, length(hists2), num)){
-        tmp <- mean(c(dist[i,][!is.na(dist[i,])], dist[i-1,][!is.na(dist[i-1,])], dist[i-2,][!is.na(dist[i-2,])]))
-        tmp <- paste(formatC(round(100*tmp,1),format="f",digits=1), sep= "")
-        avgs <- c(avgs, tmp)
-        names <- c(names, rownames(dist)[i-2])
-    }
-
-    coltmp <- rep(c(1:length(names), rep("NA", times=length(names))), times=10)
-    legend("top", col=coltmp, pch="-", cex=0.55, legend=cbind(names, avgs), ncol=2, title="\t\tAverage % Identity", bty="n")
-
     dev.off()
 }
--- nanopore/metaAnalyses/coverageSummaryPlots.R
+++ nanopore/metaAnalyses/coverageSummaryPlots.R
@@ -6,9 +6,28 @@ summary <- read.csv(args[1], header=T, row.names=1)
 name <- args[2]
 summary <- summary[order(rownames(summary)),]
 
+
+
 if (dim(summary)[1] >= 1) {
 
-    r <- 1:8
+
+    num <- 1
+    names <- vector()
+    for (i in 1:length(rownames(summary))){
+        tmp <- as.numeric(strsplit(rownames(summary)[i], "[.]")[[1]][2])
+        n <- as.character(strsplit(rownames(summary)[i], "[.]")[[1]][1])
+        if (! is.na(tmp)) {
+            num <- max(tmp, num)
+        }
+        else {
+            names <- c(names, n)
+        }
+    }
+    colmap <- vector()
+    for (i in 1:8){
+        colmap <- c(colmap, rep(i, times=num))
+    }
+    pchmap <- rep(15:(15+num-1), times=ceiling(length(hists)/num))
 
     pdf(args[3])
 
@@ -17,39 +36,39 @@ if (dim(summary)[1] >= 1) {
     ####Looking at insertion/deletion/match rates
     ################################################
     
-    plot(summary$AvgInsertionsPerReadBase, summary$AvgDeletionsPerReadBase, xlab="Avg. Insertions Per Aligned Read Base", ylab="Avg. Deletions Per Aligned Read Base", main=name, col=r, pch=c(19,18,15,17), xlim=c(0,0.2), ylim=c(0,0.2), cex.main=0.9)
-    legend("topright", cex=0.65, legend=rownames(summary), pch=c(19,18,15,17), col=r)
+    plot(summary$AvgInsertionsPerReadBase, summary$AvgDeletionsPerReadBase, xlab="Avg. Insertions Per Aligned Read Base", ylab="Avg. Deletions Per Aligned Read Base", main=name, col=colmap, pch=pchmap, xlim=c(0,0.2), ylim=c(0,0.2), cex.main=0.9, alpha=0.7)
+    legend("topright", cex=0.75, legend=names, pch="*", col=1:8)
     
     #scatterplot of Avg identity coverage vs Avg # of indels per base
-    plot(100 * summary$AvgIdentity, summary$AvgDeletionsPerReadBase + summary$AvgInsertionsPerReadBase, ylab="Avg. Indels Per Aligned Read Base", xlab="Avg. Match Identity", main=name, col=r, pch=c(19,18,15,17), xlim=c(0,100), cex.main=0.9)
-    legend("topright", cex=0.65, legend=rownames(summary), pch=c(19,18,15,17), col=r)
+    plot(100 * summary$AvgIdentity, summary$AvgDeletionsPerReadBase + summary$AvgInsertionsPerReadBase, ylab="Avg. Indels Per Aligned Read Base", xlab="Avg. Match Identity", main=name, col=colmap, pch=pchmap, xlim=c(0,100), cex.main=0.9)
+    legend("topright", cex=0.75, legend=names, pch="*", col=1:8)
 
-    q <- barplot(height=summary$AvgInsertionsPerReadBase, xaxt="n", col=r, main=paste(name,"Avg. Insertions Per Aligned Base",sep="\n"), ylab="Avg. Insertions Per Aligned Base", cex.main=0.9)
+    q <- barplot(height=summary$AvgInsertionsPerReadBase, xaxt="n", col=colmap, main=paste(name,"Avg. Insertions Per Aligned Base",sep="\n"), ylab="Avg. Insertions Per Aligned Base", cex.main=0.9)
     text(cex=0.8, x=q, y=-0.01, rownames(summary), xpd=T, srt=90)
     
-    q <- barplot(height=summary$AvgDeletionsPerReadBase, xaxt="n", col=r, main=paste(name,"Avg. Deletions Per Aligned Base",sep="\n"), ylab="Avg. Deletions Per Aligned Base", cex.main=0.9)
+    q <- barplot(height=summary$AvgDeletionsPerReadBase, xaxt="n", col=colmap, main=paste(name,"Avg. Deletions Per Aligned Base",sep="\n"), ylab="Avg. Deletions Per Aligned Base", cex.main=0.9)
     text(cex=0.8, x=q, y=-0.01, rownames(summary), xpd=T, srt=90)
     
-    q <- barplot(height=summary$AvgIdentity, xaxt="n", col=r, main=paste(name,"Identity of Aligned Bases",sep="\n"), ylab="Proportion of matched aligned bases.", cex.main=0.9)
+    q <- barplot(height=summary$AvgIdentity, xaxt="n", col=colmap, main=paste(name,"Identity of Aligned Bases",sep="\n"), ylab="Proportion of matched aligned bases.", cex.main=0.9)
     text(cex=0.8, x=q, y=-0.01, rownames(summary), xpd=T, srt=90)
     
     ################################################
     ####Looking at identity - demonstrating chaining and EM are useful improvements
     ################################################
     
-    q <- barplot(height=summary$AvgIdentity, xaxt="n", col=r, main=paste(name,"Identity (inc. gaps)",sep="\n"), ylab="Identity (inc. gaps)", cex.main=0.9)
+    q <- barplot(height=summary$AvgIdentity, xaxt="n", col=colmap, main=paste(name,"Identity (inc. gaps)",sep="\n"), ylab="Identity (inc. gaps)", cex.main=0.9)
     text(cex=0.8, x=q, y=-0.01, rownames(summary), xpd=T, srt=90)
     
     #scatterplot of Avg read coverage vs Avg match identity
-    plot(100 * summary$AvgReadCoverage, 100 * summary$AvgIdentity, ylab="Avg. Match Identity", xlab="Avg. Read Coverage", main=name, col=r, pch=c(19,18,15,17), xlim=c(0,100), ylim=c(0,100), cex.main=0.9)
-    legend("topright", cex=0.65, legend=rownames(summary), pch=c(19,18,15,17), col=r)
+    plot(100 * summary$AvgReadCoverage, 100 * summary$AvgIdentity, ylab="Avg. Match Identity", xlab="Avg. Read Coverage", main=name, col=colmap, pch=pchmap, xlim=c(0,100), ylim=c(0,100), cex.main=0.9)
+    legend("topright", cex=0.75, legend=names, pch="*", col=1:8)
 
     ################################################
     ####Looking at unmapped reads - demonstrating last is doing a great job
     ################################################
 
     #barplot of unmapped read counts per mapper
-    q <- barplot(height=summary$NumberOfMappedReads/summary$NumberOfReads, xaxt="n", col=r, main=paste(name,"Prop. Mapped Reads",sep="\n"), ylab="Prop. Reads Mapped", cex.main=0.9)
+    q <- barplot(height=summary$NumberOfMappedReads/summary$NumberOfReads, xaxt="n", col=colmap, main=paste(name,"Prop. Mapped Reads",sep="\n"), ylab="Prop. Reads Mapped", cex.main=0.9)
     text(cex=0.8, x=q, y=-0.01, rownames(summary), xpd=T, srt=90)
 
     ################################################
@@ -57,10 +76,10 @@ if (dim(summary)[1] >= 1) {
     ################################################
     
     #scatterplot of Avg. identity vs Proportion reads mapped.
-    plot(100 * summary$AvgIdentity, 100 * summary$NumberOfMappedReads/summary$NumberOfReads, xlab="Avg. Identity", ylab="Prop. Reads Mapped", main=name, col=r, pch=20, xlim=c(0,100), ylim=c(0,100), cex.main=0.9)
+    plot(100 * summary$AvgIdentity, 100 * summary$NumberOfMappedReads/summary$NumberOfReads, xlab="Avg. Identity", ylab="Prop. Reads Mapped", main=name, col=colmap, pch=20, xlim=c(0,100), ylim=c(0,100), cex.main=0.9)
     text(100 * summary$AvgIdentity, 100 * summary$NumberOfMappedReads/summary$NumberOfReads, cex=0.65, pos=2, labels=rownames(summary))
     
-    q <- barplot(height=(summary$NumberOfMappedReads/summary$NumberOfReads*summary$AvgIdentity), xaxt="n", col=r, main=paste(name,"Prop. Of Sequenced Bases Aligned with Identity",sep="\n"), ylab="Prop. Of Sequenced Bases Aligned with Identity", cex.main=0.9)
+    q <- barplot(height=(summary$NumberOfMappedReads/summary$NumberOfReads*summary$AvgIdentity), xaxt="n", col=colmap, main=paste(name,"Prop. Of Sequenced Bases Aligned with Identity",sep="\n"), ylab="Prop. Of Sequenced Bases Aligned with Identity", cex.main=0.9)
     text(cex=0.8, x=q, y=-0.01, rownames(summary), xpd=T, srt=90)
 
 
