3bfc15ad5fa553a1afcdad30901de405b48a4027
ifiddes <ian.t.fiddes@gmail.com>
2014-10-01 23:42:37 -0700

    I think this is right v2

--- nanopore/metaAnalyses/ROC_marginAlign.R
+++ nanopore/metaAnalyses/ROC_marginAlign.R
@@ -18,20 +18,18 @@ if (algorithms %% 2 == 0) {
 }
 
 
-for (page in seq(0, dim(data)[1], algorithms)) {
-    for (pos in seq(1, algorithms)) {
-        count <- 0
-        tprs <- data[seq(page+pos-10, page+pos+1, 2),]
-        fprs <- data[seq(page+pos-11, page+pos, 2),]
-        coverages <- tprs[,4]
-        algorithm <- tprs[,2][1]
-        held_out <- tprs[,3][1]
-        tprs <- tprs[,-(1:4)]
-        fprs <- fprs[,-(1:4)]
-        matplot(t(fprs), t(tprs), type="l", col=c(1:length(coverages)), main=paste("VariantCaller:\n", algorithm, "\nProportionHeldOut: ", held_out, sep=""), cex.main=0.5, cex.axis=0.5, xlab="False Positive Rate", ylab="True Positive Rate")
-        legend("topright", legend=coverages, col=c(1:length(coverages)), cex=0.35, pch="-", title="Coverage")
-        count <- count + 1
-    }
+for (page in seq(1, dim(data)[1], algorithms)) {
+    count <- 0
+    tprs <- data[seq(page, algorithms+page-1, 2),]
+    fprs <- data[seq(page+1, algorithms+page, 2),]
+    coverages <- tprs[,4]
+    algorithm <- tprs[,2][1]
+    held_out <- tprs[,3][1]
+    tprs <- tprs[,-(1:4)]
+    fprs <- fprs[,-(1:4)]
+    matplot(t(fprs), t(tprs), type="l", col=c(1:length(coverages)), main=paste("VariantCaller:\n", algorithm, "\nProportionHeldOut: ", held_out, sep=""), cex.main=0.5, cex.axis=0.5, xlab="False Positive Rate", ylab="True Positive Rate")
+    legend("topright", legend=coverages, col=c(1:length(coverages)), cex=0.35, pch="-", title="Coverage")
+    count <- count + 1
     if (count < plots) {
         while (count < plots) {
             plot.new()
