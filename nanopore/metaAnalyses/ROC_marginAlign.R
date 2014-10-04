505e3192cf3347a6415893b842a17712a0b06bf4
ifiddes <ian.t.fiddes@gmail.com>
2014-10-04 12:42:21 -0700

    fixes to ROC plots

--- nanopore/metaAnalyses/ROC_marginAlign.R
+++ nanopore/metaAnalyses/ROC_marginAlign.R
@@ -3,7 +3,8 @@
 args <- commandArgs(trailingOnly = T)
 library(lattice)
 
-data <- read.table(args[1])
+cols <- max(count.fields(args[1], sep="\t"))
+data <- read.table(args[1], fill=T, sep="\t", col.names=paste("V",seq_len(cols)))
 
 #find # of algorithms, coverages and proportions heldout used
 algorithms <- length(unique(data[,2]))
@@ -23,8 +24,8 @@ for (i in seq(1, algorithms*heldout*coverage*2, heldout*coverage*2)) {
     #this corresponds to one plot in a row
     for (j in seq(i, i+heldout*coverage*2-heldout-coverage, coverage*2)) {
         #pull out these fprs and tprs (which alternate down the whole file)
-        fprs <- data[seq(j+2, j+coverage*2-1, 2),]
-        tprs <- data[seq(j+3, j+coverage*2, 2),]
+        fprs <- data[seq(j, j+coverage*2-1, 2),]
+        tprs <- data[seq(j+1, j+coverage*2, 2),]
         #find the coverages for this plot *should always be the same*
         coverages <- tprs[,4]
         #find this trials algorithm *should be the same for each row*
@@ -36,7 +37,7 @@ for (i in seq(1, algorithms*heldout*coverage*2, heldout*coverage*2)) {
         fprs <- fprs[,-(1:4)]
         #plot and draw legend
         matplot(t(fprs), t(tprs), type="l", col=c(1:length(coverages)), main=paste("ProportionHeldOut: ", held_out, sep=""), cex.main=0.8, cex.axis=0.7, xlab="Recall", ylab="Precision")
-        legend("bottomright", legend=coverages, col=c(1:length(coverages)), cex=0.8, pch="-", title="Coverage")
+        legend("bottomleft", legend=coverages, col=c(1:length(coverages)), cex=0.8, pch="-", title="Coverage")
     }
     dev.off()
 }
