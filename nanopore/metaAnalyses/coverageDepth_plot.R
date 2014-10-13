c38aa1ab0c0df3943c365f0ea6f6a156360706b1
Miten Jain <mitenjain@users.noreply.github.com>
2014-10-13 13:45:23 -0700

    update

--- nanopore/metaAnalyses/coverageDepth_plot.R
+++ nanopore/metaAnalyses/coverageDepth_plot.R
@@ -8,7 +8,10 @@ pdf(args[2])
 
 depthFile = read.delim(inFile, sep="\t", header=F)
 
-outliers <- length(boxplot.stats(c(unlist(unlist(depthFile[3]), dpois(unlist(depthFile[3]), mean(unlist(depthFile[3]))))))$out)
+outlier_pos <- boxplot.stats(c(unlist(unlist(depthFile[3]), dpois(unlist(depthFile[3]), mean(unlist(depthFile[3]))))))$out
+write(paste(names(outlier_pos), outlier_pos), args[4])
+
+outliers <- length(outlier_pos)
 proportion <- round((outliers / length(unlist(depthFile[3]))) * 100, 2)
 
 par(mfrow <- c(1,1))
@@ -20,10 +23,10 @@ samples <- length(unlist(depthFile[3]))
 
 xmax <- max(obs_cov) + 1000
 
-x <- seq(0,max(obs_cov))
+x <- seq(min(obs_cov),max(obs_cov))
 y <- dnorm(x,mean=avg, sd=dev)
 
-hist(obs_cov, freq=F, main=paste("Coverage distribution\n Number of under-represented sites = ", outliers, "(", proportion, "%)"), xlab="Coverage", ylab="Density", xlim=c(0, xmax), col = "green")
+hist(obs_cov, freq=F, main=paste("Coverage distribution\n Number of under-represented sites = ", outliers, "(", proportion, "%)"), xlab="Coverage", ylab="Density", xlim=c(0, xmax), col = "green", breaks="FD")
 lines(x,y, lwd=1, col="red")
 legend("topleft",legend=c("Expected","Observed"),col=c("red","green"),pch="-")
 
