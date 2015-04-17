0b71ed26f377016325c096416b5bb52a88a52017
ifiddes <ian.t.fiddes@gmail.com>
2015-04-17 11:01:51 -0700

    bug in tests due to failure to check for every kmer mapping. Cannot do mappable/unmappable comparison if everything maps (or nothing maps).

--- nanopore/metaAnalyses/mappable_kmer_analysis.R
+++ nanopore/metaAnalyses/mappable_kmer_analysis.R
@@ -17,31 +17,32 @@ trial_size <- 5000
 trial_fn <- function(d, t) {
     matrix(table(factor(sample(1024, t, prob=d, replace=T), levels=1:1024)))[,1]
 }
+if (sum(data$mappableFraction) != 0 & sum(data$unmappableFraction) != 0) {
+    mappable <- replicate(num_trials, trial_fn(d=data$mappableFraction, t=trial_size))
 
-mappable <- replicate(num_trials, trial_fn(d=data$mappableFraction, t=trial_size))
+    unmappable <- replicate(num_trials, trial_fn(d=data$unmappableFraction, t=trial_size))
 
-unmappable <- replicate(num_trials, trial_fn(d=data$unmappableFraction, t=trial_size))
+    p_values <- rep(0, 1024)
 
-p_values <- rep(0, 1024)
+    for (i in 1:1024) {
+        p_values[i] <- ks.test(mappable[i,], unmappable[i,])$p.value
+    }
 
-for (i in 1:1024) {
-    p_values[i] <- ks.test(mappable[i,], unmappable[i,])$p.value
+    adjusted_p_value <- p.adjust(p_values, method="bonferroni")
+    #combine original data frame with two new vectors
+    finished <- cbind(data, p_values, adjusted_p_value)
+    #write full dataset
+    write.table(finished, outf)
+    #find significant hits
+    significant <- finished[finished$adjusted_p_value <= 0.05,]
+    pdf(outplot)
+    xyplot(finished$adjusted_p_value~finished$logFoldChange, xlab="Log Fold Change", ylab="Adjusted P Value", main="Unmappable vs. Mappable Volcano Plot")
+    dev.off()
+    #sort the significant hits by fold change
+    ordered <- significant[order(significant$logFoldChange),]
+    #report the top 20 and bottom 20 significant hits
+    top <- head(ordered, n=20L)
+    bot <- apply(tail(ordered, n=20L), 2, rev)
+
+    write.table(rbind(top,bot), outsig)
 }
-
-adjusted_p_value <- p.adjust(p_values, method="bonferroni")
-#combine original data frame with two new vectors
-finished <- cbind(data, p_values, adjusted_p_value)
-#write full dataset
-write.table(finished, outf)
-#find significant hits
-significant <- finished[finished$adjusted_p_value <= 0.05,]
-pdf(outplot)
-xyplot(finished$adjusted_p_value~finished$logFoldChange, xlab="Log Fold Change", ylab="Adjusted P Value", main="Unmappable vs. Mappable Volcano Plot")
-dev.off()
-#sort the significant hits by fold change
-ordered <- significant[order(significant$logFoldChange),]
-#report the top 20 and bottom 20 significant hits
-top <- head(ordered, n=20L)
-bot <- apply(tail(ordered, n=20L), 2, rev)
-
-write.table(rbind(top,bot), outsig)
